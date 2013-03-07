a15ed176dc7c85abc7eae81fec38fef0709f069b
Asutosh Das <asutoshd@codeaurora.org>
2013-03-07 17:43:35 +0530

    block: blk-core: add printk_ratelimit
    On failure of requests the console is flooded with prints,
    which result in a watchdog bark.
    This patch limits the number of prints to console thus, avoiding
    the bark.
    
    CRs-Fixed: 458071
    Change-Id: I614002cc1305ab7b5c6a64d278139291c1baa3c4
    Signed-off-by: Asutosh Das <asutoshd@codeaurora.org>
    
    Conflicts:
    	block/blk-core.c

--- block/blk-core.c
+++ block/blk-core.c
@@ -29,6 +29,7 @@
 #include <linux/fault-inject.h>
 #include <linux/list_sort.h>
 #include <linux/delay.h>
+#include <linux/ratelimit.h>
 #include <linux/pm_runtime.h>
 
 #define CREATE_TRACE_POINTS
@@ -2263,9 +2264,11 @@ bool blk_update_request(struct request *req, int error, unsigned int nr_bytes)
 			error_type = "I/O";
 			break;
 		}
-		printk(KERN_ERR "end_request: %s error, dev %s, sector %llu\n",
-		       error_type, req->rq_disk ? req->rq_disk->disk_name : "?",
-		       (unsigned long long)blk_rq_pos(req));
+		printk_ratelimited(
+			KERN_ERR "end_request: %s error, dev %s, sector %llu\n",
+			error_type,
+			req->rq_disk ? req->rq_disk->disk_name : "?",
+			(unsigned long long)blk_rq_pos(req));
 	}
 
 	blk_account_io_completion(req, nr_bytes);
